347b01f4e82ebac29091739b530b1d2cfe16e1ba
Josh <>
2023-06-01 18:23:21 +0800

    Finished the logic for kg to pound & pound to kg

---
+++ src/main/java/com/mycompany/converter/Converter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.converter;
+
+/**
+ *
+ * @author aaron
+ */
+public class Converter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
